e104284f09d277bede5aa478fa87a9b3bdc51959
UtilityDev <45518053+UtilityDev@users.noreply.github.com>
2020-11-25 20:57:02 +0100

    Added help files

---
+++ help.js
@@ -0,0 +1,11 @@
+let hasVisited = false;
+let secret = document.getElementById("secret");
+
+function back() {
+    location.href = 'http://127.0.0.1:5500/index.html';
+}
+
+function rickrolled() {
+    alert('Hvis du er i klasserommet, ta ned lyden...');
+    location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO';
+}
